2d065266121be84f8eb178a7839d198af758066d
lamph12 <lampv.gplaystudio@gmail.com>
2022-10-26 18:00:42 +0700

    26/10

--- Assets/Monster_HitRun/Scripts/Chest.cs
+++ Assets/Monster_HitRun/Scripts/Chest.cs
@@ -6,17 +6,31 @@ using UnityEngine.UI;
 public class Chest : MonoBehaviour
 {
     public Prize prize;
+    public PlayerManager CoinsPlayer;
     private Button yourButton;
+    public int numberCoins;
     private bool onclick= false;
-    public Text numbercoins;
-    public class Giftypessss
-    {
-        bool coins;
-        int number;
-        Sprite icon;
-    }
+    public Text numberCoinstxt;
+    public bool Coins;
+    public int id;
+    
 
-    void Start()
+    public void Init()
+    {
+        
+        Debug.Log("so lan");
+        if (Coins)
+        {
+            numberCoins = Random.Range(500, 3001);
+            numberCoinstxt.text = numberCoins.ToString();
+            
+
+        }
+        else
+        {
+        }
+    }
+    private void Start()
     {
         yourButton = GetComponent<Button>();
         yourButton.onClick.AddListener(onchest);
@@ -33,12 +47,52 @@ public class Chest : MonoBehaviour
                 prize.numberprize--;
                 Debug.Log("da mo chest");
                 //gameObject.SetActive(false);
-                gameObject.transform.GetChild(0).gameObject.SetActive(false);
-                gameObject.transform.GetChild(1).gameObject.SetActive(true);
+                
+                if (Coins)
+                {
+                    gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                    gameObject.transform.GetChild(1).gameObject.SetActive(true);
+                    gameObject.transform.GetChild(2).gameObject.SetActive(false);
+                }
+                else
+                {
+                    gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                    gameObject.transform.GetChild(1).gameObject.SetActive(false);
+                    gameObject.transform.GetChild(2).gameObject.SetActive(true);
+                }
+
             }
+            
             onclick = true;
+        }
+        
+    }
+    private void Update()
+    {
+        if(prize.numberprize <= 0)
+        {
+            StartCoroutine("endChest");
         }
 
     }
-   
+
+    IEnumerator  endChest()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (Coins)
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            gameObject.transform.GetChild(2).gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
+        }
+        prize.Chestkey.SetActive(false);
+        prize.Chestvideo.SetActive(true);
+    }
+
 }
--- Assets/Monster_HitRun/Scripts/Prize.cs
+++ Assets/Monster_HitRun/Scripts/Prize.cs
@@ -7,33 +7,45 @@ public class Prize : MonoBehaviour
 {
     private bool key;
     public int numberprize =3;
-    public List<GameObject> listchest = new List<GameObject>();
-    [SerializeField]private GameObject Chestvideo;
-    [SerializeField] private GameObject Chestkey;
-    [SerializeField] private GameObject NoThanks;
-
+    public List<Chest> listchest;
+    public GameObject Chestvideo;
+    public GameObject Chestkey;
+    public GameObject NoThanks;
+    int numberrandom;
     
 
 
     private Button nothanks;
     void Start()
     {
+        numberrandom = Random.Range(1, 10);
         nothanks = NoThanks.GetComponent<Button>();
         nothanks.onClick.AddListener(off);
-        for(int i = 0; i < listchest.Count; i++) { 
+        Debug.Log(listchest.Count);
 
+        foreach(var lis in listchest)
+        {
+            Debug.Log("kakakakakak");
+
+            if (lis.id != numberrandom)
+            {
+                Debug.Log("da vao 1");
+                lis.Coins = true;
+                lis.Init();
+            }
+            else
+            {
+                Debug.Log("da vao 2");
+                lis.Coins = false;
+                lis.Init();
+            }
         }
     }
+
+   
 
     // Update is called once per frame
-    void Update()
-    {
-        if (numberprize == 0)
-        {
-            Chestkey.SetActive(false);
-            Chestvideo.SetActive(true);
-        }
-    }
+   
     public void off()
     {
         gameObject.SetActive(false);
